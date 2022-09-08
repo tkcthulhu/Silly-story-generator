c1d9c8303547037b84289bd79988119615ad510a
Tyler Consalvi <tkconsalvi@gmail.com>
2022-09-08 14:46:59 +0000

    Logic updates

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'It was 69 celsius outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+let storyText = 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas']
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House']
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away']
@@ -16,10 +16,10 @@ randomize.addEventListener('click', result);
 
 function result() {
 
-  const newStory = storyText;
-  const xItem = randomValueFromArray();
-  const yItem = randomValueFromArray();
-  const zItem = randomValueFromArray();
+  let newStory = storyText;
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
 
   storyText = storyText.replace(':insertx:', xItem);
   storyText = storyText.replace(':inserty:', yItem);
@@ -27,15 +27,17 @@ function result() {
 
   if(customName.value !== '') {
     const name = customName.value;
-
+    storyText = storyText.replace('Bob', name);
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = Math.round(300 / 14) + ' stone';
+    const temperature =  Math.round((94 - 32) * .55) + ' centigrade';
 
+    storyText = storyText.replace('300 pounds', weight);
+    storyText = storyText.replace('94 farenheit', temperature);
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
